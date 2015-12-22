53a4626ea1b217c9ba013462a6557d938b749e71
James Halliday <mail@substack.net>
2015-12-22 13:26:25 -0500

    fix bucket for equality

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,9 +4,21 @@ var split = require('split2')
 var through = require('through2')
 var strftime = require('strftime')
 var parse = require('parse-messy-schedule')
+var parset = require('parse-messy-time')
 var minimist = require('minimist')
 
-var argv = minimist(process.argv.slice(2))
+var argv = minimist(process.argv.slice(2), {
+  boolean: [ 'm', 'w', 'd' ],
+  alias: { m: 'month', w: 'week', d: 'day' }
+})
+var time = argv._.join(' ')
+if (argv.month) {
+  time = 'every month'
+} else if (argv.week) {
+  time = 'every sunday'
+} else if (argv.day) {
+  time = 'every day'
+}
 var buckets = {}
 
 process.stdin.on('error', function () {})
@@ -17,8 +29,13 @@ process.stdin
 
 function write (line, enc, next) {
   var parts = line.toString().split(/\s+/)
-  var p = parse('every sunday')
-  var stamp = strftime('%F', p.prev(parts[0]))
+  var p = parse(time)
+  var pt = parset(parts[0])
+  var t = p.prev(pt)
+  if (p.next(t).toISOString() === pt.toISOString()) {
+    t = pt
+  }
+  var stamp = strftime('%F', t)
   if (!buckets[stamp]) buckets[stamp] = []
   next(null, stamp + ' ' + parts.slice(1).join(' ') + '\n')
 }
