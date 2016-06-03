83b7a00889101dbd24943f642a3aff727eebea85
James Halliday <mail@substack.net>
2016-06-03 13:04:03 +0100

    usage

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,17 +1,30 @@
 #!/usr/bin/env node
 var tbucket = require('../')
 var minimist = require('minimist')
+var fs = require('fs')
+var path = require('path')
 
 var argv = minimist(process.argv.slice(2), {
-  alias: { s: 'separator', d: 'delimiter' },
+  alias: {
+    h: 'help',
+    s: ['sep','separator'],
+    d: ['del','delim','delimiter']
+  },
   string: [ 'separator', 'delimiter' ],
   default: {
     separator: '/\\s+/',
     delimiter: ' '
   }
 })
+if (argv.help || argv._[0] === 'help') return usage()
+
 var timestr = argv._.join(' ')
 process.stdin.on('error', function () {})
 process.stdin
   .pipe(tbucket(timestr, argv))
   .pipe(process.stdout)
+
+function usage (code) {
+  fs.createReadStream(path.join(__dirname, 'usage.txt'))
+    .pipe(process.stdout)
+}
