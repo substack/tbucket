7ce7de48e3cd63cfe55bf946278d8ed085d5c9a7
James Halliday <mail@substack.net>
2016-06-03 10:40:22 +0100

    passing monthly test

--- test/bucket.js
+++ test/bucket.js
@@ -20,3 +20,22 @@ test('weekly', function (t) {
   b.write(' b\n2016-06-05 c\n2016-06-13 d')
   b.write('\n2016-07-04 e\n2016-06-06 f\n')
 })
+
+test('monthly', function (t) {
+  var expected = [
+    '2016-06-01 a',
+    '2016-05-01 b',
+    '2016-06-01 c',
+    '2016-06-01 d',
+    '2016-07-01 e',
+    '2016-06-01 f'
+  ]
+  t.plan(expected.length)
+  var b = tbucket('monthly')
+  b.pipe(split()).on('data', function (buf) {
+    t.equal(buf.toString(), expected.shift())
+  })
+  b.write('2016-06-01 a\n2016-05-29')
+  b.write(' b\n2016-06-05 c\n2016-06-13 d')
+  b.write('\n2016-07-04 e\n2016-06-06 f\n')
+})
